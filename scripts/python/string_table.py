5587400c614ced2a1221070df99e61d8ab74be59
nfactorial <nfactorial@btopenworld.com>
2016-02-18 18:53:01 +0000

    Formatting updates.

--- scripts/python/string_table.py
+++ scripts/python/string_table.py
@@ -14,38 +14,47 @@
 # limitations under the License.
 #
 
-# Provides support for creating a string table used by the ngen game engine.
-# A string table is a memory block that contains a collection of ASCII strings.
-# This table is used by various objects in the ngen data files.
-
 
 class ngenStringTable:
+    """
+    Provides support for creating a string table used by the ngen game engine.
+    A string table is a memory block that contains a collection of ASCII strings.
+    This table is used by various objects in the ngen data files.
+    """
     def __init__( self ):
         self.rawStrings = []
         self.stringDictionary = {}
         self.stringOffset = 0
 
-    def len( self ):
-        return len( self.rawStrings )
+    def __len__(self):
+        """
+        :return: The number of strings contained within the string table.
+        """
+        return len(self.rawStrings)
 
     # We use string offset rather than item index, this allows
     # objects to refer to items without needing an index to
     # offset table.
 
-    def addString( self, newString ):
-        if newString == None:
+    def addString(self, newString):
+        if newString is None:
             return 0xFFFFFFFF
-        itemOffset = self.stringDictionary.get( newString )
+        itemOffset = self.stringDictionary.get(newString)
         if itemOffset is None:
             itemOffset = self.stringOffset
-            self.rawStrings.append( newString )
-            self.stringDictionary[ newString ] = itemOffset
-            self.stringOffset += len( newString ) + 1   # +1 because of null terminator
+            self.rawStrings.append(newString)
+            self.stringDictionary[newString] = itemOffset
+            self.stringOffset += len(newString) + 1   # +1 because of null terminator
         return itemOffset
 
-    def export( self, binaryWriter ):
-        if len( self.rawStrings ) > 0:
-            binaryWriter.beginChunk( 'S', 'T', 'T', 'B' )
+    def export(self, binaryWriter):
+        """
+        Writes the contents of the string table to the specified binary data file.
+        :param binaryWriter: The writer to be used when exporting the table.
+        :return: Nothing
+        """
+        if len(self.rawStrings) > 0:
+            binaryWriter.beginChunk('S', 'T', 'T', 'B')
             for exportString in self.rawStrings:
-                binaryWriter.writeAscii( exportString )
+                binaryWriter.writeAscii(exportString)
             binaryWriter.endChunk()
